37f4e42b925ed7e474847936ba15057603fe08ea
URIEL SUTI <49194950@ort>
2025-07-10 15:35:06 -0300

    modificación suti (70%)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,29 +18,43 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Reiniciar()
+    {
+        HttpContext.Session.Remove("id");
+        return RedirectToAction(Index);
+    }
+
+    [HttpPost]
     public IActionResult Login(string email, string contraseña)
     {
         int idd = BD.Login(email, contraseña);
         HttpContext.Session.SetString("id", idd.ToString());
         ViewBag.Id = idd;
-        return View("Index");
+        return View("Index2");
     }
 
-    public IActionResult SelectIntegrante(int dni)
+    public IActionResult Index2()
+    {
+        int id = int.Parse(HttpContext.Session.GetString("id"));
+        ViewBag.Id = id;
+        return View();
+    }
+
+    public IActionResult SelectIntegrante()
     {
         int id = int.Parse(HttpContext.Session.GetString("id"));
         ViewBag.Id = id;
         return View("infoDatosPersonales");
     }
 
-    public IActionResult MostrarDatosFamiliares(int dni)
+    public IActionResult MostrarDatosFamiliares()
     {
         int id = int.Parse(HttpContext.Session.GetString("id"));
         ViewBag.Id = id;
         return View("infoDatosFamiliares");
     }
 
-    public IActionResult MostrarDatosIntereses(int dni)
+    public IActionResult MostrarDatosIntereses()
     {
         int id = int.Parse(HttpContext.Session.GetString("id"));
         ViewBag.Id = id;
